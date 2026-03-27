1b48c82bcb560a475aabb5d3f5d155c03095f9ee
Hollow-graphic <120960592+Hollow-graphic@users.noreply.github.com>
2026-03-27 15:15:23 +0100

    Add files via upload

---
+++ temp2.bat
@@ -0,0 +1,45 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+for /f %%a in ('echo prompt $E ^| cmd') do set ESC=%%a
+set RED=%ESC%[31m
+set YELLOW=%ESC%[33m
+set BLUE=%ESC%[34m
+set RESET=%ESC%[0m
+
+:startup
+set /A nbCables=2 + !RANDOM! * 4 / 32768 + 1
+
+for /l %%i in (1,1,!nbCables!) do (
+    set /A listColor[%%i]=!RANDOM! * 4 / 32768 + 1
+    call :intToColor !listColor[%%i]!
+    echo %%i!color!~~~~~~~!RESET!
+)
+pause
+set /A response="(x to exit) which cables to you cut ?:"
+call :verifyCables !response!
+goto startup
+
+:intToColor
+if "%~1"=="1" (
+    set color=!RED!
+) else if "%~1"=="2" (
+    set color=!YELLOW!
+) else if "%~1"=="3" (
+    set color=!BLUE!
+) else (
+    set color=!RESET!
+)
+exit /b
+
+REM S'il n'y a pas de fil rouge, coupez le deuxième fil.
+REM Sinon, si le dernier fil est blanc, coupez le dernier fil.
+REM Sinon, s'il y a plus d'un fil bleu, coupez le dernier fil bleu.
+REM Sinon, coupez le dernier fil.
+:verifyCables
+if !nbCables!=3 (
+	
+) else (
+	echo undifined rules
+)
+exit /b
